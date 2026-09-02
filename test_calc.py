a86bd4cf4d87a7f155f3905dc97f180da3b6ecae
jaronharness <jaron.harness01@gmail.com>
2026-09-02 01:47:57 -0500

    Resolve flake8 linting errors in calc.py and test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -4,29 +4,36 @@ from calc import add
 def test_add_positive_numbers():
     assert add(2,3) == 5
 
+
 def test_add_negative_numbers():
     assert add(-1, -4) == -5
     assert add(5, -3) == 2
     assert add(-7, 4) == -3
 
+
 def test_add_zero():
     assert add(0, 7) == 7
     assert add(12, 0) == 12
 
+
 def test_add_floats():
     assert add(1.2, 3.6) == pytest.approx(4.8)
 
+
 def test_add_large_numbers():
     assert add(10**12, 12**14) == 10**12 + 12**14
+
 
 def test_add_type_error_string():
     with pytest.raises(TypeError):
         add("2", 5)
 
+
 def test_add_type_error_none():
     with pytest.raises(TypeError):
         add(None, 9)
 
+
 def test_add_type_error_list():
     with pytest.raises(TypeError):
-        add([1,2 ,3], 6)
+        add([1 ,2 ,3], 6)
